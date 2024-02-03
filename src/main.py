7e5c4062b6138245276f6e32c87c7d99d9039d3c
whowhat <jakobthefoodmonster@gmail.com>
2024-02-03 20:31:55 +0100

    basic file going.

---
+++ src/main.py
@@ -0,0 +1,11 @@
+import sys, time, random
+import pyaudio
+from scipy.io.wavfile import read
+
+def main() -> None:
+    print('time for some crazy fx')
+
+if __name__ == '__main__':
+    main()
+
+
